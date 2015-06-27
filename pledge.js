f41943b801683499a3d3f71ecd15c505505b6f6c
Nirmohi <nirmohi0605@gmail.com>
2015-06-26 22:23:59 -0400

    passed ch1 specs

--- pledge.js
+++ pledge.js
@@ -23,16 +23,43 @@ var pledge = require('pledge');
 var myDeferral = pledge.defer();
 var myPromise1 = myDeferral.$promise;
 --------------------------------------------------------*/
-function $Promise() {
 
-}
+function $Promise () {
+    this.state = "pending";
+    this.value = undefined;
+    this.handlerGroups = [];
+};
 
-function Deferral() {
-
-}
+function Deferral () {
+    this.$promise = new $Promise;
+};
 
 var defer = function() {
-	var defferal =  new Deferral();
-	defferal.$promise = new $Promise();
-	return defferal;
+    return new Deferral(); 
 };
+
+Deferral.prototype.resolve = function(data) {
+    if (data && this.$promise.state === 'pending') {
+        this.$promise.value = data;
+        this.$promise.state = "resolved";
+    }
+    else if (this.$promise.state === 'pending'){
+    this.$promise.state = "resolved";
+    }
+}
+
+Deferral.prototype.reject = function (err) {
+    if (err && this.$promise.state === 'pending') {
+        this.$promise.value = err;
+        this.$promise.state = "rejected";
+    }
+    else if (this.$promise.state === 'pending'){
+    this.$promise.state = "rejected"
+    }
+}
+
+
+
+
+
+
