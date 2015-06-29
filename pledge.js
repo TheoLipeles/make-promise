9ed8799f249a102cb0eb2aafa31b4cd0c24d1107
Nirmohi <nirmohi0605@gmail.com>
2015-06-29 10:42:23 -0400

    chap 3 completed

--- pledge.js
+++ pledge.js
@@ -24,40 +24,39 @@ var myDeferral = pledge.defer();
 var myPromise1 = myDeferral.$promise;
 --------------------------------------------------------*/
 
-function $Promise () {
+function $Promise() {
     this.state = "pending";
     this.value = undefined;
     this.handlerGroups = [];
 }
 
-function Deferral () {
+function Deferral() {
     this.$promise = new $Promise();
     this.$promise.deferral = this;
 }
 
 var defer = function() {
-    return new Deferral(); 
+    return new Deferral();
 };
 
 Deferral.prototype.resolve = function(data) {
     if (data && this.$promise.state === 'pending') {
         this.$promise.value = data;
         this.$promise.state = "resolved";
-    }
-    else if (this.$promise.state === 'pending'){
-    this.$promise.state = "resolved";
+    } else if (this.$promise.state === 'pending') {
+        this.$promise.state = "resolved";
     }
     this.$promise.callHandlers();
 };
 
-Deferral.prototype.reject = function (err) {
+Deferral.prototype.reject = function(err) {
     if (err && this.$promise.state === 'pending') {
         this.$promise.value = err;
         this.$promise.state = "rejected";
+    } else if (this.$promise.state === 'pending') {
+        this.$promise.state = "rejected";
     }
-    else if (this.$promise.state === 'pending'){
-    this.$promise.state = "rejected";
-    }
+    this.$promise.callHandlers();
 };
 
 $Promise.prototype.then = function(successCb, errorCb) {
@@ -80,21 +79,34 @@ $Promise.prototype.then = function(successCb, errorCb) {
 $Promise.prototype.callHandlers = function() {
     for (var h = 0; h < this.handlerGroups.length; h++) {
         handler = this.handlerGroups[h];
-        if(this.state === "resolved") {
-            if(handler.state === "pending") {
-                if(handler.successCb) {
+        if (this.state === "resolved") {
+            if (handler.state === "pending") {
+                if (handler.successCb) {
                     handler.successCb(this.value);
                     handler.state = "resolved";
                 }
-                if(handler.errorCb) {
+
+            }
+        } else if (this.state === 'rejected') {
+            if (handler.state === 'pending') {
+                if (handler.errorCb) {
                     handler.errorCb(this.value);
                     handler.state = "resolved";
                 }
             }
         }
     }
+
+    for (var h = 0; h < this.handlerGroups.length; h++) {
+        if (this.handlerGroups[h].state === 'resolved') {
+            this.handlerGroups.splice(h, 1);
+        }
+
+    }
+
 };
 
 
-
-
+$Promise.prototype.catch = function(errorHandler) {
+    this.then(null, errorHandler);
+};
